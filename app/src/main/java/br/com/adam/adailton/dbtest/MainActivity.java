871ed489b32d20b56c22a0d3b07c2a7dbc3c5d87
adailtonadam <adailtonadam@gmail.com>
2014-07-29 12:52:07 -0300

    Utilizando libs locais.
    1- criar um projeto qualquer.
    2- Adicionar um módulo do tipo library. Crie a biblioteca. Após compilar, será gerado um arquivo aar.
    
    3- Para utilizar a lib em outro projeto, sem a necessidade de recriar a lib, basta copiar a lib, arquivo aar, para o diretório libs e incluir as linhas abaixo no arquivo build.gradle do projeto.
    
    repositories {
        flatDir {
            dirs 'libs'
        }
    }
    dependencies {
        compile(name: 'AdamLib', ext: 'aar')
    }

--- app/src/main/java/br/com/adam/adailton/dbtest/MainActivity.java
+++ app/src/main/java/br/com/adam/adailton/dbtest/MainActivity.java
@@ -2,9 +2,13 @@ package br.com.adam.adailton.dbtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -36,8 +40,88 @@ public class MainActivity extends Activity {
     }
 
 
+    public void onButtonStartTest(View v) {
+        testDb();
+    }
 
-    public void onButtonStartTest(View v){
 
+    public void testDb() {
+        Table1Dao dao1 = new Table1Dao(this.getApplicationContext());
+        dao1.deleteAll();
+
+        Table2Dao dao2 = new Table2Dao(this.getApplicationContext());
+        dao2.deleteAll();
+
+        Table3LinkedDao dao3 = new Table3LinkedDao(this.getApplicationContext());
+        dao3.deleteAll();
+
+        Table1 table1 = new Table1();
+        table1.setName("item1");
+        table1.setOtherField("otherfield1");
+        table1.setFloatField(1F);
+        table1.setLongField(1);
+        long table1Id = dao1.insert(table1);
+
+        if (table1Id != 1) {
+            Toast.makeText(this, "teste nok table1 insert", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Table2 table2 = new Table2();
+        table2.setDescription("description1");
+        long table2Id = dao2.insert(table2);
+
+        if (table2Id != 1) {
+            Toast.makeText(this, "teste nok table2 insert", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Table3Linked table3 = new Table3Linked();
+        table3.setTable1Id(table1Id);
+        table3.setTable2Id(table2Id);
+        long table3Id = dao3.insert(table3);
+        if (table3Id != 1) {
+            Toast.makeText(this, "teste nok table3 insert", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        List<Table1> listTable1 = dao1.getAllOrderBy();
+        List<Table2> listTable2 = dao2.getAllOrderBy();
+        List<Table3Linked> listTable3 = dao3.getAll();
+
+        String out = "";
+
+        out +=listTable1.get(0).getId() + ": " +
+                listTable1.get(0).getName() + ": " +
+                listTable1.get(0).getOtherField() + ": " +
+                listTable1.get(0).getLongField() + ": " +
+                listTable1.get(0).getFloatField()+",";
+
+        out += listTable2.get(0).getId() + ": " +
+                listTable2.get(0).getDescription()+",";
+
+        out += listTable3.get(0).getId() + ": " +
+                listTable3.get(0).getTable1Id() + ": " +
+                listTable3.get(0).getTable2Id();
+        /*Log.d("Debug",out);
+        Log.d("Debug", listTable1.get(0).getId() + ": " +
+                listTable1.get(0).getName() + ":" +
+                listTable1.get(0).getOtherField() + ": " +
+                listTable1.get(0).getLongField() + ": " +
+                listTable1.get(0).getFloatField());
+
+        Log.d("Debug", listTable2.get(0).getId() + ": " +
+                listTable2.get(0).getDescription());
+
+        Log.d("Debug", listTable3.get(0).getId() + ": " +
+                listTable3.get(0).getTable1Id() + ": " +
+                listTable3.get(0).getTable2Id());
+        */
+
+        if(out.compareTo("1: item1: otherfield1: 1: 1.0,1: description1,1: 1: 1") == 0) {
+            Toast.makeText(this, "teste ok", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "teste Nok", Toast.LENGTH_LONG).show();
+        }
     }
 }
